fb9da23ed0b57fc7e9df3e39b435ed341a542c72
Fathur Danu <fathurdanu@gmail.com>
2024-06-15 22:15:27 +0700

    v0.0.9

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from "@elysiajs/cors";
 const app = new Elysia();
 app.use(cors())
 
-app.get("/", () => "Hello Elysia v0.0.8")
+app.get("/", () => "Hello Elysia v0.0.9")
 
 app.get("/countries", ({ query: { key_words } }) => {
   if (key_words) {
